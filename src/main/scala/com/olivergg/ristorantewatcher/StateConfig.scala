735303c9c6db256f9ddb3257ead4a20ae8f24d4a
Olivier <olivergg@users.noreply.github.com>
2014-09-21 22:09:36 +0200

    Creation of a IState implementation.

--- src/main/scala/com/olivergg/ristorantewatcher/StateConfig.scala
+++ src/main/scala/com/olivergg/ristorantewatcher/StateConfig.scala
@@ -2,12 +2,22 @@ package com.olivergg.ristorantewatcher
 
 import com.olivergg.ionic.StateProviderAware
 import com.olivergg.ionic.UrlRouterProviderAware
+import scala.scalajs.js.Dynamic.{ literal => lit }
+import com.olivergg.ionic.IState
 
 object StateConfig extends StateProviderAware with UrlRouterProviderAware {
 
   override def initialize() {
-    
+
     //TODO :
     //stateProvider.state("tab", config)
+    stateProvider.state("tab", new State(
+      url = "/tab",
+      `abstract` = true,
+      templateUrl = "templates/tabs.html"
+    ))
+
   }
 }
+
+class State(url: String, `abstract`: Boolean, templateUrl: String) extends IState
