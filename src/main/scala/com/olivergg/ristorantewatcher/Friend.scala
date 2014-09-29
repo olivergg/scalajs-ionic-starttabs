beb126529c9105f9bd1d4fe33953e39f63a68939
Olivier <olivergg@users.noreply.github.com>
2014-09-29 18:27:41 +0200

    Use @JSExportAll instead of @JSExport.

--- src/main/scala/com/olivergg/ristorantewatcher/Friend.scala
+++ src/main/scala/com/olivergg/ristorantewatcher/Friend.scala
@@ -2,6 +2,6 @@ package com.olivergg.ristorantewatcher
 
 import scala.scalajs.js
 import com.greencatsoft.angularjs.InjectionTarget
-import scala.scalajs.js.annotation.JSExport
-@JSExport
+import scala.scalajs.js.annotation.JSExportAll
+@JSExportAll
 case class Friend(id: Int, name: String)
