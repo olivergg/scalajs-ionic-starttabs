25170a8c6cb9c58c9018be1585ae259133186db6
Olivier <olivergg@users.noreply.github.com>
2014-10-15 23:21:25 +0200

    - Add to the fastOptJS task a dependency to the fullClassPath in Runtime in order to instantiate the Index class. - Create ionic specific tags for ScalaTags - Remove commented code.

---
+++ src/main/scala/com/olivergg/Index.scala
@@ -0,0 +1,25 @@
+package com.olivergg
+import scalatags.Text.all._
+import com.olivergg.ionic.IonicHtmlTags._
+class Index {
+  //TODO FIXME : implement the complete index.html file.
+  def frag(param: String) = html(
+    head(
+      meta(charset := "utf-8"),
+      meta(name := "viewport", content := "initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width"),
+
+      script("some script")
+    ),
+    body(
+      h1(span("This is my title "),span(param)),
+      ionNavBar("test"),
+      div(
+        p("This is my first paragraph"),
+        p("This is my second paragraph")
+      )
+    )
+  )
+  println("this is just a test string printed out to check if this class can be instantiated from an SBT task (using introspection)")
+  println("this should be write to a file ")
+  println(frag("someParam"))
+}
