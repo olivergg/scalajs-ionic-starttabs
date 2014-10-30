6a461db1f134f2938edf0d68fd28c9334a4b6dcb
Olivier <olivergg@users.noreply.github.com>
2014-10-30 22:00:34 +0100

    Update TODOs.

--- src/main/scala/com/olivergg/ristorantewatcher/PlatformInitializer.scala
+++ src/main/scala/com/olivergg/ristorantewatcher/PlatformInitializer.scala
@@ -16,19 +16,8 @@ object PlatformInitializer extends Runnable {
   override def initialize() {
     ionicPlatform.ready { () =>
       println("ionicPlatform is ready")
-      /// FIXME : unsuccessful attemp to use cordova plugins from the browser (it should work on a real device though) 
-      // should use Cordova API bindings.
-      //      if (!g.window.cordova.isInstanceOf[Unit] && !g.window.cordova.plugins.Keyboard.isInstanceOf[Unit]) {
-      //      val cordova: UndefOr[js.Dynamic] = g.window.cordova.asInstanceOf[UndefOr[js.Dynamic]]
-      //      val cordovaPluginsKeyboard: UndefOr[js.Dynamic] = g.window.Keyboard.asInstanceOf[UndefOr[js.Dynamic]]
-      //      if (cordova.isDefined && cordovaPluginsKeyboard.isDefined) {
-      //        cordovaPluginsKeyboard.get.hideKeyboardAccessoryBar(true)
-      //      }
-
-      //      if (!g.window.StatusBar.isInstanceOf[Unit]) {
-      //        // org.apache.cordova.statusbar required
-      //        g.window.StatusBar.styleDefault();
-      //      }
+      //TODO : use ngCordova to communicate with the cordova API in an angular way
+      //TODO : find a way to use cordova plugins with ionic serve in a web browser.
     }
   }
 }
