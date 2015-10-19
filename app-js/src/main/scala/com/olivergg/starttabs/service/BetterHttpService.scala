95eb8c5a8e39487c50387519e674989b255e4ca3
Nikolay Kushin <nikolay@indoo.rs>
2015-10-19 23:37:33 +0200

    - code cleanup - ionic update - fixed android build

--- app-js/src/main/scala/com/olivergg/starttabs/service/BetterHttpService.scala
+++ app-js/src/main/scala/com/olivergg/starttabs/service/BetterHttpService.scala
@@ -1,33 +1,25 @@
 package com.olivergg.starttabs.service
 
 import com.greencatsoft.angularjs.core.HttpService
-import com.greencatsoft.angularjs.injectable
-import com.greencatsoft.angularjs.inject
-import com.olivergg.ionic.IonicLoading
-import scala.scalajs.concurrent.JSExecutionContext.Implicits.runNow
-import prickle.Unpickler
+import com.greencatsoft.angularjs.{Factory, Service, injectable}
+import com.olivergg.ionic.{IonicLoading, LoadingOpt}
+import prickle.{Unpickle, Unpickler}
+
 import scala.concurrent.Future
-import scala.util.Try
+import scala.scalajs.concurrent.JSExecutionContext.Implicits.runNow
 import scala.scalajs.js.JSON
-import prickle.Unpickle
-import scala.util.Success
-import scala.util.Failure
-import scala.scalajs.js
-import com.olivergg.ionic.LoadingOpt
-import com.greencatsoft.angularjs.Factory
-import com.greencatsoft.angularjs.Service
+import scala.util.{Failure, Success, Try}
 
 @injectable("$betterhttpService")
 class BetterHttpService(http: HttpService, loading: IonicLoading) extends Service {
 
   def getJsonAndUnpickle[T: Unpickler](url: String): Future[T] = {
     loading.show(LoadingOpt("Loading..."))
-    val getFuture: Future[js.Any] = http.get(url) // implicit conversion occurs here.
+    val getFuture = http.get(url) // implicit conversion occurs here.
     getFuture.onFailure {
-      case err => {
+      case err =>
         loading.hide()
         loading.show(LoadingOpt("Please check your network connection", duration = 3000))
-      }
     }
     val intermediateFuture: Future[Try[T]] = getFuture.map(JSON.stringify(_)).map(Unpickle[T].fromString(_))
     val outFuture: Future[T] = intermediateFuture.flatMap {
